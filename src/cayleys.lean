7b6fc2b5f7200627a4a771004ae1b3da3358899a
Theo <tc4618@ic.ac.uk>
2020-07-14 22:44:24 +0100

    proved diagram commutes

--- src/cayleys.lean
+++ src/cayleys.lean
@@ -9,8 +9,6 @@ universes u v
 namespace equiv.perm
 open equiv function subgroup
 
-variables {α : Type u}
-
 lemma perms_eq_fun_eq {X : Type u} (p : perm X) (q : perm X) (h : p.to_fun = q.to_fun) : p = q := 
 begin 
    apply perm.ext,
@@ -70,5 +68,50 @@ def lift_to_perm (G : Type*) [group G] : G →* perm G := {
    map_one' := by {ext, simp},
    map_mul' := λ g h, by {ext, simp [mul_assoc _ _ _]},
 }
+
+variables {G₁ : Type*} {G₂ : Type*} [group G₁] [group G₂]
+
+def ψ (h : G₁ ≃* G₂): perm G₁ → perm G₂ := λ θ, 
+{
+   to_fun := h.1 ∘ θ.1 ∘ h.2,
+   inv_fun := h.1 ∘ θ.2 ∘ h.2,
+   left_inv := begin
+      intro x,
+      calc (h.to_fun ∘ θ.inv_fun ∘ h.inv_fun) ((h.to_fun ∘ θ.to_fun ∘ h.inv_fun) x) 
+            = (h.to_fun ∘ θ.inv_fun ∘ (h.inv_fun ∘ h.to_fun) ∘ θ.to_fun ∘ h.inv_fun) x : rfl 
+        ... = (h.to_fun ∘ θ.inv_fun ∘ id ∘ θ.to_fun ∘ h.inv_fun) x : by rw left_inverse.id h.3
+        ... = (h.to_fun ∘ (θ.inv_fun ∘ θ.to_fun) ∘ h.inv_fun) x : rfl 
+        ... = (h.to_fun ∘ id ∘ h.inv_fun) x : by rw left_inverse.id θ.3
+        ... = (h.to_fun ∘ h.inv_fun) x : rfl 
+        ... = id x : by rw right_inverse.id h.4,
+   end,
+   right_inv := begin 
+      intro x,
+     calc (h.to_fun ∘ θ.to_fun ∘ h.inv_fun) ((h.to_fun ∘ θ.inv_fun ∘ h.inv_fun) x)
+            = (h.to_fun ∘ θ.to_fun ∘ (h.inv_fun ∘ h.to_fun) ∘ θ.inv_fun ∘ h.inv_fun) x : rfl 
+        ... = (h.to_fun ∘ θ.to_fun ∘ id ∘ θ.inv_fun ∘ h.inv_fun) x : by rw left_inverse.id h.3
+        ... = (h.to_fun ∘ (θ.to_fun ∘ θ.inv_fun) ∘ h.inv_fun) x : rfl 
+        ... = (h.to_fun ∘ id ∘ h.inv_fun) x : by rw right_inverse.id θ.4 
+        ... = (h.to_fun ∘ h.inv_fun) x : rfl 
+        ... = id x : by rw right_inverse.id h.4,
+   end,
+}
+
+lemma embedding_diagram_com (h : G₁ ≃* G₂) 
+  : (ψ h) ∘ (lift_to_perm G₁).1 = (lift_to_perm G₂).1 ∘ h.1 := begin 
+   ext g x,
+   have H₁ : ((ψ h ∘ (lift_to_perm G₁).to_fun) g) x = (h.1 g) * x,
+     calc ((ψ h ∘ (lift_to_perm G₁).to_fun) g) x 
+         = h.1 (g * (h.2 x)) : rfl 
+     ... = (h.1 g) * (h.1 (h.2 x)) : mul_equiv.map_mul h g (h.inv_fun x) 
+     ... = (h.1 g) * ((h.1 ∘ h.2) x) : rfl 
+     ... = (h.1 g) * (id x) : by rw right_inverse.id h.4,
+   have H₂ : (((lift_to_perm G₂).to_fun ∘ h.1) g) x = (h.1 g) * x,
+      refl,
+   rwa [H₁, H₂],
+end 
+
+
+-- (h.1 ∘ (λ x, g * x) ∘ h.2) x = h.1 (g * (h.2 x))
 
 end equiv.perm
